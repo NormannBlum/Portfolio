758487ff1456bab1713acf90f66b45426b29dc17
Normann Blum <normannblum@gmail.com>
2025-01-19 10:28:52 +0100

    add components routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainContentComponent},
+];
+  
