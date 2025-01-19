ba6a2f25324c0ef1c61859b7d0bac2f811cd4a78
Normann Blum <normannblum@gmail.com>
2025-01-19 10:04:33 +0100

    add portfolio components

---
+++ src/app/main-content/portfolio/epl/epl.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epl',
+  standalone: true,
+  imports: [],
+  templateUrl: './epl.component.html',
+  styleUrl: './epl.component.scss'
+})
+export class EplComponent {
+
+}
