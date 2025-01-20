058e5bdbb8eca2dc5f5ec4862fc35586b8749f7e
Normann Blum <normannblum@gmail.com>
2025-01-20 14:01:26 +0100

    add mixins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from "./shared/components/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
