99a4047612e54800db2bdbe330458ffce7d8df92
Normann Blum <normannblum@gmail.com>
2025-01-23 12:42:44 +0100

    about me new style

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroComponent } from "./hero/hero.component";
-import { AboutmeComponent } from "./aboutme/aboutme.component";
-import { SkillsComponent } from "./skills/skills.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { ContactComponent } from "./contact/contact.component";
-import { ReferencesComponent } from "./references/references.component";
-import { EplComponent } from "./portfolio/epl/epl.component";
-import { JoinComponent } from "./portfolio/join/join.component";
-import { PokedexComponent } from "./portfolio/pokedex/pokedex.component";
+import { HeroComponent } from './hero/hero.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReferencesComponent } from './references/references.component';
+import { EplComponent } from './portfolio/epl/epl.component';
+import { JoinComponent } from './portfolio/join/join.component';
+import { PokedexComponent } from './portfolio/pokedex/pokedex.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, HeroComponent, AboutmeComponent, SkillsComponent, PortfolioComponent, ContactComponent, ReferencesComponent, EplComponent, JoinComponent, PokedexComponent],
+  imports: [
+    CommonModule,
+    HeroComponent,
+    AboutmeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    ReferencesComponent,
+    EplComponent,
+    JoinComponent,
+    PokedexComponent,
+  ],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
