9abcf580e68cacba9431ff06ef722ea6b5e334a7
Normann Blum <normannblum@gmail.com>
2025-02-03 22:48:10 +0100

    header portfolio and aboutme fixes

--- src/app/main-content/aboutme/aboutme.component.ts
+++ src/app/main-content/aboutme/aboutme.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './aboutme.component.html',
-  styleUrl: './aboutme.component.scss'
+  styleUrl: './aboutme.component.scss',
 })
 export class AboutmeComponent {
-
+  scrollToSection(event: Event, sectionId: string) {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
