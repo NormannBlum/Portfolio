534175664087c7ec15a58ac2315cf4bce7f277ee
Normann Blum <normannblum@gmail.com>
2025-02-05 11:36:43 +0100

    contact form validation fixes

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
