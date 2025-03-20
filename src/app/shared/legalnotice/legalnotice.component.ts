39902fe77800116ef749d5ed68dc8df4f1ec47a0
Normann Blum <normannblum@gmail.com>
2025-03-20 14:39:41 +0100

    fix: some changes on legalnotice & implementing imprint

--- src/app/shared/legalnotice/legalnotice.component.ts
+++ src/app/shared/legalnotice/legalnotice.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-legalnotice',
   standalone: true,
   imports: [],
   templateUrl: './legalnotice.component.html',
-  styleUrl: './legalnotice.component.scss'
+  styleUrl: './legalnotice.component.scss',
 })
-export class LegalnoticeComponent {
+export class LegalnoticeComponent implements OnInit {
 
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 }
