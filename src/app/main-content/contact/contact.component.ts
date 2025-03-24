624e22c1740f6fbd65495cd9b7d6cca9184c69ab
Normann Blum <normannblum@gmail.com>
2025-03-24 08:55:56 +0100

    feat: implement ngx-translate for contact section

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -3,16 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, RouterLink, CommonModule],
+  imports: [FormsModule, RouterLink, CommonModule, TranslatePipe],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
   http = inject(HttpClient);
+
+  constructor(public translate: TranslateService) {}
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
 
   contactData = {
     name: '',
@@ -34,24 +45,6 @@ export class ContactComponent {
       },
     },
   };
-
-  // onSubmit(ngForm: NgForm) {
-  //   if (ngForm.submitted && ngForm.form.valid) {
-  //     this.http
-  //       .post(this.post.endPoint, this.post.body(this.contactData))
-  //       .subscribe({
-  //         next: (response) => {
-  //           ngForm.resetForm();
-  //         },
-  //         error: (error) => {
-  //           console.error(error);
-  //         },
-  //         complete: () => console.info('send post complete'),
-  //       });
-  //   } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-  //     ngForm.resetForm();
-  //   }
-  // }
 
   onSubmit(ngForm: NgForm) {
     if (ngForm.submitted && ngForm.form.valid) {
--- src/app/shared/legalnotice/legalnotice.component.ts
+++ src/app/shared/legalnotice/legalnotice.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legalnotice',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './legalnotice.component.html',
   styleUrl: './legalnotice.component.scss',
 })
 export class LegalnoticeComponent implements OnInit {
+    constructor(public translate: TranslateService) {}
+  
+    changeLanguage(language: string) {
+      this.translate.use(language);
+    }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
