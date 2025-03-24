ab9ef06029a88d11c842c55a08f687f39bf9e8f5
Normann Blum <normannblum@gmail.com>
2025-03-24 07:36:28 +0100

    feat: implement ngx-translate for my-skills section

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
+  constructor(public translate: TranslateService) {}
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
+
   scrollToSection(event: Event, sectionId: string) {
     event.preventDefault();
     const element = document.getElementById(sectionId);
