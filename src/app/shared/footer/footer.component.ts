58db771338f4d77e1572d7f524e64e147daae510
Normann Blum <normannblum@gmail.com>
2025-03-24 10:44:50 +0100

    feat: implement ngx-translate for legal notice and footer section

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  constructor(public translate: TranslateService) {}
+  
+    changeLanguage(language: string) {
+      this.translate.use(language);
+    }
+
+
   scrollToTop(): void {
     if (
       window.location.pathname === '/' ||
